222f3110343ad424487c933f12f2be008be5b533
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-07-22 11:29:46 -0600

    Update pairwise.js

--- JS/pairwise.js
+++ JS/pairwise.js
@@ -4,10 +4,13 @@ function pairwise(arr, arg) {
     for(var j=i+1; j<arr.length; j++) {
       if(arr[i] + arr[j] == arg) {
         sum = sum + i + j
+        arr.splice(i,1)
+        arr.splice(j,1)
       }
     }
   }
   return sum
 }
 
-console.log(pairwise([1,4,2,3,0,5], 7));
+console.log(pairwise([1,4,2,3,0,5], 7))
+console.log(pairwise([1, 1, 1], 2))
